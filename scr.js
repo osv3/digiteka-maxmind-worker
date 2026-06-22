fad3af2c08ee1745166c4916d07168070c9485d9
Osvaldas Smailys <osvaldas@digiteka.lt>
2026-06-22 11:28:07 +0300

    update v2

--- scr.js
+++ scr.js
@@ -1,5 +1,12 @@
 const calculate = (a,b) => {
     return a+b
 };
+const arr =[5,2,3,4,1,5]
+
+const sumArr = (a) => {
+    return a.reduce((a,b) => a+b)
+}
 
 console.log(calculate(20,35))
+
+console.log(sumArr(arr))
